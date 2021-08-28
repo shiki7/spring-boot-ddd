6a5b22ace3fe88639146a475b4109a7475923db9
shiki <bitbite8@gmail.com>
2021-08-28 11:27:49 +0900

    Update SampleRepositoryImpl.java

--- src/main/java/com/example/springbootddd/infrastructure/repository/sample/SampleRepositoryImpl.java
+++ src/main/java/com/example/springbootddd/infrastructure/repository/sample/SampleRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.example.springbootddd.infrastructure.repository.sample;
 
 import com.example.springbootddd.domain.repository.sample.SampleRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class SampleRepositoryImpl implements SampleRepository {
