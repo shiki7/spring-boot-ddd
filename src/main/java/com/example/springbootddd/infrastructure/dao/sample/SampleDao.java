5759fc6ffe625f127433b4f9625a2f7ba2e99ee7
shiki <bitbite8@gmail.com>
2021-08-28 11:45:28 +0900

    Update SampleDao.java

--- src/main/java/com/example/springbootddd/infrastructure/dao/sample/SampleDao.java
+++ src/main/java/com/example/springbootddd/infrastructure/dao/sample/SampleDao.java
@@ -1,1 +1,16 @@
+package com.example.springbootddd.infrastructure.dao.sample;
+
+import org.seasar.doma.Dao;
+import org.seasar.doma.Select;
+import org.seasar.doma.boot.ConfigAutowireable;
+
+import java.util.List;
+
+@Dao
+@ConfigAutowireable
+public interface SampleDao {
+
+    @Select
+    List<String> selectSampleData();
+}
 
