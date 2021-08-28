a30d17ca85adbc125209476eb85ed5b19b307d4e
shiki <bitbite8@gmail.com>
2021-08-28 11:34:06 +0900

    Update SampleRepositoryImpl.java

--- src/main/java/com/example/springbootddd/infrastructure/repository/sample/SampleRepositoryImpl.java
+++ src/main/java/com/example/springbootddd/infrastructure/repository/sample/SampleRepositoryImpl.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class SampleRepositoryImpl implements SampleRepository {
 
+    // daoを呼び出す
+    
     @Override
     public String getData() {
+        
+        // DBやAPIからデータを取得、登録処理を記載
+        
         return "string";
     }
 }
