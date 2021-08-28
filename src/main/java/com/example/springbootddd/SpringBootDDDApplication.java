375b9e32637bc5b4dd830038f92910a3cf7ce5c1
shiki <bitbite8@gmail.com>
2021-08-28 11:25:25 +0900

    Update SpringBootDDDApplication.java

--- src/main/java/com/example/springbootddd/SpringBootDDDApplication.java
+++ src/main/java/com/example/springbootddd/SpringBootDDDApplication.java
@@ -1,5 +1,8 @@
 package com.example.springbootddd;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringBootDDDApplication {
     public static void main(String[] args) {
